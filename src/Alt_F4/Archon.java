7fa9aa6eae8d0bf7d16e8cb3508e9d0b240e90e6
Sheldon Burks <sheldon.burks@gmail.com>
2017-01-10 18:26:37 -0500

    Added gardener spawn to its own method

--- src/Alt_F4/Archon.java
+++ src/Alt_F4/Archon.java
@@ -6,18 +6,23 @@ import battlecode.common.GameActionException;
 
 public class Archon extends Base {
     public static void run() throws GameActionException {
+        System.out.println("Archon Spawned");
+
         while (true) {
             try {
-                if (rc.canHireGardener(Direction.getNorth())) {
-                    rc.hireGardener(Direction.getNorth());
-                }
-
+                spawnGardener();
                 Pathing.tryMove(Pathing.randomDirection());
 
                 Clock.yield();
             } catch (Exception e) {
                 System.out.println(e.getMessage());
             }
+        }
+    }
+
+    public static void spawnGardener() throws GameActionException {
+        if (rc.canHireGardener(Direction.getNorth())) {
+            rc.hireGardener(Direction.getNorth());
         }
     }
 }
