29bad8b9ed4efa5417f765d5b6733b2485cafa3c
Sheldon Burks <sheldon.burks@gmail.com>
2017-01-12 16:00:07 -0500

    Added static variable that contains the locations of our archons and the enemy archons

--- src/Alt_F4/Base.java
+++ src/Alt_F4/Base.java
@@ -1,16 +1,24 @@
 package Alt_F4;
 
+import battlecode.common.MapLocation;
 import battlecode.common.RobotController;
 
 public class Base {
     public static int numberOfArchons;
     public static int numberOfEnemyArchons;
+    public static MapLocation[] archonLocations;
+    public static MapLocation[] enemyArchonLocations;
+
     protected static RobotController rc;
 
     public static void init(RobotController rc)
     {
         Base.rc = rc;
-        numberOfArchons = rc.getInitialArchonLocations(rc.getTeam()).length;
-        numberOfEnemyArchons = rc.getInitialArchonLocations(rc.getTeam().opponent()).length;
+
+        archonLocations = rc.getInitialArchonLocations(rc.getTeam());
+        enemyArchonLocations = rc.getInitialArchonLocations(rc.getTeam().opponent());
+
+        numberOfArchons = archonLocations.length;
+        numberOfEnemyArchons = enemyArchonLocations.length;
     }
 }
