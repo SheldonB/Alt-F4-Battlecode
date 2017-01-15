f29c60e1e0307291898a40693e05e3d5d1896816
Sheldon Burks <sheldon.burks@gmail.com>
2017-01-15 17:13:18 -0500

    Added a message class that contains message constants that will eb used for memory mapping the broadcast array

---
+++ src/Alt_F4/Message.java
@@ -0,0 +1,22 @@
+package Alt_F4;
+
+public class Message {
+    /*
+    Define message channels that will be used.
+    The broadcasting array will be memory mapped using
+    a range of channels, for a unit group. Specific channels
+    will also be used for specific a specific purpose.
+     */
+    public static final int STRATEGY_CHANNEL = 0;
+    public static final int GARDENER_COUNT_CHANNEL = 1;
+
+    public static final int ARCHON_VOTING_START_CHANNEL = 1;
+    public static final int ARCHON_VOTING_END_CHANNEL = 4;
+
+    /*
+    Define message constants that are to be passed into
+    the message array.
+     */
+    public static final int TURTLE_MESSAGE = 1;
+    public static final int SCOUT_RUSH_MESSAGE = 2;
+}
