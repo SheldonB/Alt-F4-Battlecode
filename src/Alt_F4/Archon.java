fcca223d07669174ef51bbc8a5c8019a4ea20f3d
Sheldon Burks <sheldon.burks@gmail.com>
2017-01-15 17:15:18 -0500

    Added Archon Strategy voting on initial rounds, to determine proper strategy

--- src/Alt_F4/Archon.java
+++ src/Alt_F4/Archon.java
@@ -1,19 +1,30 @@
 package Alt_F4;
 
-import battlecode.common.Clock;
-import battlecode.common.Direction;
-import battlecode.common.GameActionException;
+import battlecode.common.*;
+
+import java.util.Arrays;
+
 
 public class Archon extends Base {
-    private static int spawnedUnitCount = 0 ;
+    private static final int GARDENERS_TO_SPAWN = 7;
+    private static int spawnedUnitCount = 0;
 
     public static void run() throws GameActionException {
         System.out.println("Archon Spawned");
 
         while (true) {
             try {
+                if (rc.getRoundNum() == 1) {
+                    VoteMapStrategy();
+                } else if (rc.getRoundNum() == 2) {
+                    DetermineMapStrategy();
+                }
+
+                Debug.debug_drawSensorRadius();
+                Debug.debug_drawStrideRadius();
+
                 spawnGardener();
-                //Pathing.tryMove(Pathing.randomDirection());
+
                 Utils.CheckWinConditions();
                 Clock.yield();
             } catch (Exception e) {
@@ -22,14 +33,68 @@ public class Archon extends Base {
         }
     }
 
+    /*
+    This function checks to see if the current map has a valid
+    turtle strategy. A valid turtle startegy comes from a map
+    that has 2 or more archons, and a proper turtle gardner
+    hexagon can be built.
+     */
+    public static boolean CheckValidTurtleMap() {
+        if (numberOfArchons == 1) {
+            return false;
+        }
+
+        TreeInfo[] sensedTrees = rc.senseNearbyTrees(rc.getType().sensorRadius, Team.NEUTRAL);
+        return sensedTrees.length <= 1;
+    }
+
+
+    private static void VoteMapStrategy() throws GameActionException {
+        int[] hashedLocations = new int[numberOfArchons];
+        for (int i = 0; i < numberOfArchons; i++) {
+            hashedLocations[i] = Utils.mapLocationToInt(archonLocations[i]);
+        }
+
+        Arrays.sort(hashedLocations);
+
+        int offset = 0;
+        for (int i = 0; i < numberOfArchons; i++) {
+            if (hashedLocations[i] == Utils.mapLocationToInt(rc.getLocation())) {
+                offset = i;
+                break;
+            }
+        }
+
+        if (CheckValidTurtleMap()) {
+            rc.broadcast(Message.ARCHON_VOTING_START_CHANNEL + offset, Message.TURTLE_MESSAGE);
+        } else {
+            rc.broadcast(Message.ARCHON_VOTING_START_CHANNEL + offset, Message.SCOUT_RUSH_MESSAGE);
+        }
+    }
+
+    private static void DetermineMapStrategy() throws GameActionException {
+        int aggregate = 0;
+        for (int i = 0; i < numberOfArchons; i++) {
+            aggregate += rc.readBroadcast(Message.ARCHON_VOTING_START_CHANNEL + i);
+        }
+
+        if (aggregate != numberOfArchons) {
+            rc.broadcast(Message.STRATEGY_CHANNEL, Message.SCOUT_RUSH_MESSAGE);
+            return;
+        }
+
+        rc.broadcast(Message.STRATEGY_CHANNEL, Message.TURTLE_MESSAGE);
+    }
+
     public static void spawnGardener() throws GameActionException {
         Direction buildDirection = Direction.getNorth();
         buildDirection = buildDirection.rotateRightDegrees(60 * spawnedUnitCount);
-        if (rc.canHireGardener(buildDirection) && spawnedUnitCount < 7) {
-            rc.broadcast(0, rc.readBroadcast(0) + 1);
+
+        if (rc.canHireGardener(buildDirection) && spawnedUnitCount < GARDENERS_TO_SPAWN) {
+            rc.broadcast(Message.GARDENER_COUNT_CHANNEL, rc.readBroadcast(0) + 1);
             rc.hireGardener(buildDirection);
             spawnedUnitCount++;
-        } else if (spawnedUnitCount < 7 && !rc.canHireGardener(buildDirection)) {
+        } else if (spawnedUnitCount < GARDENERS_TO_SPAWN && !rc.canHireGardener(buildDirection)) {
             Direction dir = Pathing.randomDirection();
             if (rc.canHireGardener(dir)) {
                 rc.hireGardener(dir);
