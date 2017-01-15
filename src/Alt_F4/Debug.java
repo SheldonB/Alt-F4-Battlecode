b971fbc32f5da171de71933de387687bc63c762a
Sheldon Burks <sheldon.burks@gmail.com>
2017-01-15 17:12:09 -0500

    Debugging class with useful methods

---
+++ src/Alt_F4/Debug.java
@@ -0,0 +1,27 @@
+package Alt_F4;
+
+import battlecode.common.Direction;
+import battlecode.common.MapLocation;
+
+public class Debug extends Base {
+
+    public static void debug_drawSensorRadius() {
+        debug_drawCircle(15, rc.getType().sensorRadius, 255, 0, 0);
+    }
+
+    public static void debug_drawStrideRadius() {
+        debug_drawCircle(45, rc.getType().strideRadius, 0, 0, 255);
+    }
+
+    private static void debug_drawCircle(int degreeOffset, float radius, int red, int green, int blue) {
+        Direction dir = Direction.getEast();
+        float currentDegrees = dir.getAngleDegrees();
+
+        while (currentDegrees <= 360) {
+            MapLocation loc = rc.getLocation().add(dir.rotateRightDegrees(currentDegrees), radius);
+            rc.setIndicatorDot(loc, red, green, blue);
+            currentDegrees += degreeOffset;
+        }
+    }
+
+}
