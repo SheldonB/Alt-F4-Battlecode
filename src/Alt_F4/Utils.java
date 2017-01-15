211fd5f1fee3657df87c72f4aced68aac8c01bca
Sheldon Burks <sheldon.burks@gmail.com>
2017-01-15 17:14:26 -0500

    Added hashing and dehashing for map locations, so they can be broadcasted properly. The hashing and dehashing, does not take into account the floating point numbers, so this will need to be fixed in the future.

--- src/Alt_F4/Utils.java
+++ src/Alt_F4/Utils.java
@@ -2,6 +2,7 @@ package Alt_F4;
 
 import battlecode.common.GameActionException;
 import battlecode.common.GameConstants;
+import battlecode.common.MapLocation;
 
 public class Utils extends Base {
     public static boolean CheckWinConditions() throws GameActionException {
@@ -10,5 +11,17 @@ public class Utils extends Base {
             return true;
         }
         return false;
+    }
+
+    public static int mapLocationToInt(MapLocation loc)  {
+        if (loc == null) return 0xfffff;
+        return (((int)loc.x) << 10) | ((int)loc.y);
+    }
+
+    public static MapLocation mapLocationFromInt(int data) {
+        if ((data & 0xfffff) == 0xfffff) return null;
+        int x = ((data & 0xffc00) >>> 10);
+        int y = (data & 0x003ff);
+        return new MapLocation(x, y);
     }
 }
