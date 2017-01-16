c836e24a1a02771b22becb58679937c8c8e86c6e
Sheldon Burks <sheldon.burks@gmail.com>
2017-01-16 10:02:02 -0500

    Implemented part of Scout rush to travel to archons and shoot

--- src/Alt_F4/Scout.java
+++ src/Alt_F4/Scout.java
@@ -1,20 +1,82 @@
 package Alt_F4;
 
-import battlecode.common.Clock;
-import battlecode.common.GameActionException;
-import battlecode.common.RobotInfo;
+import battlecode.common.*;
 
 public class Scout extends Base {
+    private static MapLocation targetLocation;
+
     public static void run() throws GameActionException {
         while (true) {
             try {
-                harassClosestEnemy();
+                if (rc.readBroadcast(Message.SCOUT_ATTACK_COORD_CHANNEL) == 0) {
+                    targetLocation = determineRushLocation();
+                    rc.broadcast(Message.SCOUT_ATTACK_COORD_CHANNEL, Utils.mapLocationToInt(targetLocation));
+                }
 
+                if (targetLocation == null) {
+                    targetLocation = Utils.mapLocationFromInt(rc.readBroadcast(Message.SCOUT_ATTACK_COORD_CHANNEL));
+                }
+
+                if (targetLocation != null) {
+                    rc.setIndicatorLine(rc.getLocation(), targetLocation, 255, 0, 0);
+                }
+
+                if (rc.getLocation().distanceTo(targetLocation) > GameConstants.BULLET_SPAWN_OFFSET) {
+                    Pathing.tryMove(rc.getLocation().directionTo(targetLocation));
+                }
+
+                collectBullets();
+                targetLocation = determinePriorityTarget();
+                tryFireOnTarget(targetLocation);
                 Clock.yield();
             } catch (Exception e) {
                System.out.println(e.getMessage());
             }
         }
+    }
+
+    static void tryFireOnTarget(MapLocation loc) throws GameActionException {
+        if (rc.canFireSingleShot()) {
+            rc.fireSingleShot(rc.getLocation().directionTo(targetLocation));
+        }
+    }
+
+    static MapLocation determinePriorityTarget() throws GameActionException {
+        RobotInfo[] sensedRobots = rc.senseNearbyRobots(rc.getType().sensorRadius, rc.getTeam().opponent());
+
+        for (RobotInfo robot : sensedRobots) {
+            if (robot.getType() == RobotType.ARCHON) {
+                return robot.getLocation();
+            }
+        }
+        return targetLocation;
+    }
+
+    static MapLocation determineRushLocation() throws GameActionException {
+        MapLocation closestArchonLocation = enemyArchonLocations[0];
+        for (MapLocation loc : enemyArchonLocations) {
+            if (rc.getLocation().distanceTo(loc) < rc.getLocation().distanceTo(closestArchonLocation)) {
+                closestArchonLocation = loc;
+            }
+        }
+
+        return closestArchonLocation;
+    }
+
+    static void collectBullets() throws GameActionException {
+        TreeInfo[] sensedTrees = rc.senseNearbyTrees(rc.getType().sensorRadius, Team.NEUTRAL);
+
+        for (TreeInfo tree : sensedTrees) {
+            if (rc.canShake(tree.getID())) {
+                rc.shake(tree.getID());
+                break;
+            }
+        }
+    }
+
+    static void wander() {
+        Direction dir = enemyArchonLocations[0].directionTo(rc.getLocation());
+        rc.setIndicatorLine(rc.getLocation(), enemyArchonLocations[0], 255, 0, 0);
     }
 
     static void harassClosestEnemy() throws GameActionException {
@@ -26,7 +88,6 @@ public class Scout extends Base {
                     closestEnemy = enemy;
                 }
             }
-
             Pathing.tryMove(rc.getLocation().directionTo(closestEnemy.getLocation()));
             rc.fireSingleShot(rc.getLocation().directionTo(closestEnemy.getLocation()));
         }
