62e4c8f7fa3cf95999b2af30fce677e4cf2d914b
Sheldon Burks <sheldon.burks@gmail.com>
2017-01-24 15:06:44 -0500

    Added smart shooting to soldier code

--- src/Alt_F4/Soldier.java
+++ src/Alt_F4/Soldier.java
@@ -1,8 +1,10 @@
 package Alt_F4;
 
-import battlecode.common.GameActionException;
+import battlecode.common.*;
 
 class Soldier extends Base {
+    private static BodyInfo target;
+
     static void run() throws GameActionException {
         System.out.println("Solder has spawned.");
 
@@ -10,9 +12,62 @@ class Soldier extends Base {
             try {
                 Utils.CheckWinConditions();
                 Base.update();
+
+                if (!rc.hasMoved() && nearbyBullets.length > 0) {
+                    Pathing.tryDodgeBullet();
+                }
+
+                target = determineTarget();
+
+                if (determineTarget() != null) {
+                    tryFireOnTarget();
+                }
+
+                if (!rc.hasMoved()) {
+                    Pathing.tryMove(Pathing.randomDirection());
+                }
+
+                Clock.yield();
             } catch (Exception e) {
                 System.out.println(e);
             }
         }
     }
+
+
+    static BodyInfo determineTarget() throws GameActionException {
+        for (RobotInfo enemyRobot : visibleEnemyUnits) {
+            if(!willShotCollideWithBody(enemyRobot)) {
+                rc.setIndicatorLine(rc.getLocation(), enemyRobot.getLocation(), 0, 255, 0);
+                return enemyRobot;
+            }
+        }
+        return null;
+    }
+
+    static boolean willShotCollideWithBody(BodyInfo robotTarget) {
+        Vector toEnemy = new Vector(rc.getLocation(), robotTarget.getLocation());
+
+        for (RobotInfo friendlyRobot : visibleFriendlyUnits) {
+            Vector toFriendly = new Vector(rc.getLocation(), friendlyRobot.getLocation());
+            Vector projection = Vector.projection(toFriendly, toEnemy);
+
+            if (projection.getEndPoint().distanceTo(friendlyRobot.getLocation()) <= friendlyRobot.getRadius()){
+                System.out.println("Not firing, because will hit unit");
+                return true;
+            }
+        }
+        return false;
+    }
+
+    static boolean tryFireOnTarget() throws GameActionException {
+        if (rc.canFireSingleShot()) {
+            rc.fireSingleShot(rc.getLocation().directionTo(target.getLocation()));
+            System.out.println("Firing bullet");
+            return true;
+        }
+        return false;
+    }
+
 }
+
