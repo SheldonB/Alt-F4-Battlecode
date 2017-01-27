52358bc84e9d24a37c25c407ca23417cb7386a2b
Sheldon Burks <sheldon.burks@gmail.com>
2017-01-26 19:29:44 -0500

    Added a constants file for constants that are used across all files. File specific constants should be declared in their specific class

---
+++ src/Alt_F4/Constants.java
@@ -0,0 +1,8 @@
+package Alt_F4;
+
+class Constants {
+    static final int SOLDIERS_FOR_AGGRESSION = 2;
+
+    static final int LUMBERJACK_SPAWN_OFFSET = 75;
+    static final int SOLDIER_SPAWN_OFFSET = 150;
+}
