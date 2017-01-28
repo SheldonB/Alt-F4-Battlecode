9a83a656e7407cbf3b8ce28464922eb980ba35d1
Sheldon Burks <sheldon.burks@gmail.com>
2017-01-28 15:34:47 -0600

    Uped the spawn offsets of lumberjacks and soldiers

--- src/Alt_F4/Constants.java
+++ src/Alt_F4/Constants.java
@@ -1,8 +1,6 @@
 package Alt_F4;
 
 class Constants {
-    static final int SOLDIERS_FOR_AGGRESSION = 2;
-
-    static final int LUMBERJACK_SPAWN_OFFSET = 75;
-    static final int SOLDIER_SPAWN_OFFSET = 150;
+    static final int LUMBERJACK_SPAWN_OFFSET = 100;
+    static final int SOLDIER_SPAWN_OFFSET = 175;
 }
