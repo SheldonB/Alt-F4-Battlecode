ac42b45602272239717d6ae92e6339e9cdf4187c
Sheldon Burks <sheldon.burks@gmail.com>
2017-01-30 12:26:06 -0500

    Added a round sent to the packet, and seperated x and y cords to there own getter

--- src/Alt_F4/Packet.java
+++ src/Alt_F4/Packet.java
@@ -3,16 +3,18 @@ package Alt_F4;
 import battlecode.common.MapLocation;
 
 public class Packet {
-    public static final int PACKET_SIZE = 3;
+    public static final int PACKET_SIZE = 5;
 
     private int packetType;
     private int robotId;
     private MapLocation loc;
+    private int roundSent;
 
-    public Packet(int packetType, int robotId, MapLocation loc) {
+    public Packet(int packetType, int robotId, MapLocation loc, int roundSent) {
         this.packetType = packetType;
         this.robotId = robotId;
         this.loc = loc;
+        this.roundSent = roundSent;
     }
 
     public int getPacketType() {
@@ -23,7 +25,19 @@ public class Packet {
         return this.robotId;
     }
 
-    public int getLocation() {
-        return Utils.mapLocationToInt(loc);
+    public MapLocation getLocation() {
+        return this.loc;
+    }
+
+    public float getXCord() {
+        return loc.x;
+    }
+
+    public float getYCord() {
+        return loc.y;
+    }
+
+    public int getRoundSent() {
+        return this.roundSent;
     }
 }
