ec2968c92f05b312c7c88261299879b73dbf27b6
Sheldon Burks <sheldon.burks@gmail.com>
2017-01-30 12:26:32 -0500

    Packets are now offset to 5 channels. Added enemy locations channels

--- src/Alt_F4/Message.java
+++ src/Alt_F4/Message.java
@@ -7,42 +7,39 @@ public class Message {
     a range of channels, for a unit group. Specific channels
     will also be used for specific a specific purpose.
      */
-    public static final int ARCHON_GENERAL_CHANNEL_START = 0;
-    public static final int ARCHON_GENERAL_CHANNEL_END = 999;
+    public static final int ARCHON_GENERAL_CHANNEL_START = 1;
+    public static final int ARCHON_GENERAL_CHANNEL_END = 1000;
 
-    public static final int GARDENER_GENERAL_CHANNEL_START = 1000;
-    public static final int GARDENER_GENERAL_CHANNEL_END = 1998;
+    public static final int GARDENER_GENERAL_CHANNEL_START = 1001;
+    public static final int GARDENER_GENERAL_CHANNEL_END = 2000;
 
-    public static final int SCOUT_GENERAL_CHANNEL_START = 2000;
-    public static final int SCOUT_GENERAL_CHANNEL_END = 2997;
+    public static final int SCOUT_GENERAL_CHANNEL_START = 2001;
+    public static final int SCOUT_GENERAL_CHANNEL_END = 3000;
 
-    public static final int LUMBERJACK_GENERAL_CHANNEL_START = 3000;
-    public static final int LUMBERJACK_GENERAL_CHANNEL_END = 3996;
+    public static final int LUMBERJACK_GENERAL_CHANNEL_START = 3001;
+    public static final int LUMBERJACK_GENERAL_CHANNEL_END = 4000;
 
-    public static final int SOLDIER_GENERAL_CHANNEL_START = 4000;
-    public static final int SOLDIER_GENERAL_CHANNEL_END = 4995;
+    public static final int SOLDIER_GENERAL_CHANNEL_START = 4001;
+    public static final int SOLDIER_GENERAL_CHANNEL_END = 5000;
 
-    public static final int TANK_GENERAL_CHANNEL_START = 5000;
-    public static final int TANK_GENERAL_CHANNEL_END = 5994;
+    public static final int TANK_GENERAL_CHANNEL_START = 5001;
+    public static final int TANK_GENERAL_CHANNEL_END = 6000;
 
-    public static final int STRATEGY_CHANNEL = 6000;
+    public static final int ENEMY_LOCATION_CHANNEL_START = 6001;
+    public static final int ENEMY_LOCATION_CHANNEL_END = 7000;
 
-    public static final int ARCHON_VOTING_START_CHANNEL = 6001;
-    public static final int ARCHON_VOTING_END_CHANNEL = 6004;
+    public static final int STRATEGY_CHANNEL = 7001;
 
-    public static final int TOTAL_TREE_COUNT_CHANNEL = 6005;
-    public static final int GARDENER_COUNT_CHANNEL = 6006;
-    public static final int LUMBERJACK_COUNT_CHANNEL = 6007;
-    public static final int SOLDIER_COUNT_CHANNEL = 6008;
-    public static final int SCOUT_COUNT_CHANNEL = 6009;
-    public static final int TANK_COUNT_CHANNEL = 6010;
+    public static final int TOTAL_TREE_COUNT_CHANNEL = 7002;
+    public static final int GARDENER_COUNT_CHANNEL = 7003;
+    public static final int LUMBERJACK_COUNT_CHANNEL = 7004;
+    public static final int SOLDIER_COUNT_CHANNEL = 7005;
+    public static final int SCOUT_COUNT_CHANNEL = 7006;
+    public static final int TANK_COUNT_CHANNEL = 7007;
 
-    public static final int LAST_KNOW_ENEMY_ARCHON_CHANNEL = 6011;
+    public static final int LAST_KNOW_ENEMY_ARCHON_CHANNEL = 7008;
 
-    public static final int SCOUT_ATTACK_COORD_CHANNEL = 6012;
-    public static final int SCOUT_ATTACK_TARGET_ID_CHANNEL = 6013;
-
-    public static final int MAX_KNOWN_X_INT_LOCATION_CHANNEL = 6014;
+    public static final int MAX_KNOWN_X_INT_LOCATION_CHANNEL = 7009;
     public static final int MAX_KNOWN_X_FLOAT_LOCATION_CHANNEL = MAX_KNOWN_X_INT_LOCATION_CHANNEL + 1;
 
     public static final int MIN_KNOWN_X_INT_LOCATION_CHANNEL = MAX_KNOWN_X_FLOAT_LOCATION_CHANNEL + 1;
